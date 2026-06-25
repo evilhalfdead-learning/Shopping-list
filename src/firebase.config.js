a426906e2938ed82a39c9e8be4a2d00daeadb248
evilhalfdead-learning <evilhalfdead@gmail.com>
2026-06-25 16:25:19 -0400

    firebase config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,17 +1,19 @@
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics';
 import { getDatabase } from 'firebase/database'
 import { getAuth, signInAnonymously } from 'firebase/auth'
 
 // Replace these with your Firebase config from Firebase Console
 // Get your config: Firebase Console > Project Settings > Your apps
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-  databaseURL: 'https://YOUR_PROJECT_ID-default-rtdb.firebaseio.com',
+  apiKey: "AIzaSyDyi1gY7ciFaIMoXwW6NxC7J9IHDqKcn2w",
+  authDomain: "shopping-list-app-1e599.firebaseapp.com",
+  projectId: "shopping-list-app-1e599",
+  storageBucket: "shopping-list-app-1e599.firebasestorage.app",
+  messagingSenderId: "342623243120",
+  appId: "1:342623243120:web:66049578302d4f1263ddb5",
+  measurementId: "G-MZNFPQY5W5",
+  databaseURL: 'https://shopping-list-app-1e599-default-rtdb.firebaseio.com/',
 }
 
 // Initialize Firebase
@@ -22,6 +24,7 @@ export const database = getDatabase(app)
 
 // Initialize Auth
 export const auth = getAuth(app)
+export const analytics = getAnalytics(app);
 
 // Sign in anonymously on app load
 export const initializeAuth = async () => {
